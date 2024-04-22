8bde1d675983f4bfb2ce4b66bde1e98e7e253ffd
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-22 11:44:28 +0200

    last commit

--- myscript/script.js
+++ myscript/script.js
@@ -1,45 +1,23 @@
-// console.log(12 % 4);
-// console.log(1 % 3);
 
 const item = document.querySelector('ul');
 
 
 
-for (let z = 1; z < 43; z++) {
-    if (z % 3 == 0) {
-        // document.querySelector('li').innerHTML = "si zio";
-        console.log("si zio");
+for (let z = 1; z <= 100; z++) {
+    let valore = z;
+    let colore = "style=background-color:#F2F2F7;";
+    if (z % 3 == 0 && z % 5 == 0) {
+        valore = `fizz buzz`
+        colore = "style=background-color:#EC407A;"
     }
-    else {
-        // console.log(`${numero} non e multiplo di 3`)
-        console.log("no zio");
-        // document.querySelector('li').innerHTML = "no zio";
+    else if (z % 3 == 0) {
+        valore = `fizz`
+        colore = "style=background-color:#3DE770;"
     }
-    item.innerHTML = item.innerHTML + `
-         <li data-value ="${z}">
-         ${z}
-         </li>
-         `;
+    else if (z % 5 == 0) {
+        valore = `buzz`
+        colore = "style=background-color:#0D68D8;"
+    }
+    item.innerHTML = item.innerHTML + `<li ${colore}>${valore}</li>`;
 }
-// let numero = document.querySelector('li').getAttribute('data-value');
-// console.log(numero);
-
-
-
-// for (let z = 1; z < 101; z++) {
-// const newp = document.createElement('li');
-// newp.append(z);
-// item.append(newp);
-// const numero = document.querySelector('li').getAttribute
-// console.log(numero);
-
-// if (numero % 0 == 0) {
-// document.querySelector('li').innerHTML = "si zio";
-// }
-// else {
-// // console.log(`${numero} non e multiplo di 3`)
-// document.querySelector('li').innerHTML = "no zio";
-
-// }
-// }
 
